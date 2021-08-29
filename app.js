0a2f38e09549bb4a6f56d4427a771c032bc1ea64
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-08-29 20:20:30 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ function closeNav() {
 		navlinks.style.width = '0%'
 	}
 }
+
+
+
+// Scroll top function
+const scrollBtn = document.querySelector('#scrollBtn');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if(window.innerWidth > 800) {
+	  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	    scrollBtn.style.display = "block";
+	  } else {
+	    scrollBtn.style.display = "none";
+	  }
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+  	document.documentElement.scrollTop = 0;
+}
